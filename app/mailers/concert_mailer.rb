031134bc705e9c4aa2b46dfb15df88985c89f07c
Nelly Duclos <knopfler42@yahoo.fr>
2017-12-16 14:11:07 +0100

    mailer

--- app/mailers/concert_mailer.rb
+++ app/mailers/concert_mailer.rb
@@ -2,9 +2,10 @@ class ConcertMailer < ApplicationMailer
 
   default from: "concert@mountain-men.fr"
 
-  def new_dates(concerts, users)
+  def new_dates(concerts)
     @concerts = Concert.last_found
-    mail( to:  users.pluck[:email] ,
+    @users = User.all
+    mail( to:  @users.find_each { |u| u.email },
          subject:  "Des nouvelles dates pour les Mountain Men"
         )
   end
