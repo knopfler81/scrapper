9d5b60a07c0be3adcf7050708ffcbeb0b680fbbd
Nelly Duclos <knopfler42@yahoo.fr>
2017-12-16 14:17:16 +0100

    fix mail pour tous les users

--- app/mailers/concert_mailer.rb
+++ app/mailers/concert_mailer.rb
@@ -1,11 +1,12 @@
 class ConcertMailer < ApplicationMailer
 
   default from: "concert@mountain-men.fr"
+  USERS = User.all
 
   def new_dates(concerts)
     @concerts = Concert.last_found
-    @users = User.all
-    mail( to:  @users.find_each { |u| u.email },
+
+    mail( to:  USERS.find_each { |u| u.email },
          subject:  "Des nouvelles dates pour les Mountain Men"
         )
   end
