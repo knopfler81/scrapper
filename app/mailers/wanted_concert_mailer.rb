0890a88b40090dcf2872f9d9c5d0dcbacca10cc8
Nelly Duclos <knopfler42@yahoo.fr>
2017-12-17 13:20:54 +0100

    fixed!

--- app/mailers/wanted_concert_mailer.rb
+++ app/mailers/wanted_concert_mailer.rb
@@ -2,10 +2,16 @@ class WantedConcertMailer < ApplicationMailer
 
 default from: "concert@mountain-men.fr"
 
-  def matching_locations(wanted)
 
-    mail( to:   "knopfler42@yahoo.fr",
-         subject:  "Ils arrivent vers chez toi !"
-        )
-  end
+
+  def matching_locations(wanted)
+    #wanteds =  WantedConcert.where("department = ?", wanted)
+    user = wanted.user_id
+    User.where(id: user).each do |user|
+
+      mail( to: user.email,
+           subject:  "Ils arrivent vers chez toi !"
+          )
+    end
+ end
 end
