02f39e8e153547514e218415ff6500b970b98e11
Nelly Duclos <knopfler42@yahoo.fr>
2018-03-03 18:02:51 +0100

    modif des flash message et suppression des responds to

--- app/controllers/wanted_concerts_controller.rb
+++ app/controllers/wanted_concerts_controller.rb
@@ -13,12 +13,10 @@ class WantedConcertsController < ApplicationController
   def create
     @wanted_concert = WantedConcert.new(wanted_concerts_params)
     @wanted_concert.user_id = current_user.id
-    respond_to do |format|
-      if @wanted_concert.save
-        format.html { redirect_to wanted_concerts_path, notice: "Ton souhait est bien enregistré" }
-      else
-        format.html { render :new , alert: "Oups recommence!" }
-      end
+    if @wanted_concert.save!
+      redirect_to wanted_concerts_path, notice: "Ton souhait est bien enregistré"
+    else
+      render :new , alert: "Oups recommence!"
     end
   end
 
